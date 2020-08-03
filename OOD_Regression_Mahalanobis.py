5e8749e3fab426a70d90443b040acddf70d334ab
Anas Awadalla <anasa2@cs.washington.edu>
2020-08-03 12:54:04 -0700

    added ood regression function

--- OOD_Regression_Mahalanobis.py
+++ OOD_Regression_Mahalanobis.py
@@ -15,66 +15,27 @@ from sklearn.linear_model import LogisticRegressionCV
 # args = parser.parse_args()
 # print(args)
 
-def train_detector():
+def train_detector(dataset_list, out_list):
     net_type = "model"
-    dataset_list = []
-    out_list = []
-    
-    # initial setup
-    dataset_list = ['cifar10', 'cifar100', 'svhn']
+    outf = "/output/"
 
-    if args.net_type == 'densenet121':
-        dataset_list = ['ham10000']
-    elif args.net_type == "parkinsonsNet-rest":
-        dataset_list = ['mpower-rest'] 
-    elif args.net_type == "parkinsonsNet-return":
-        dataset_list = ['mpower-return']
-    elif args.net_type == "parkinsonsNet-outbound":
-        dataset_list = ['mpower-outbound']   
     score_list = ['Mahalanobis_0.0', 'Mahalanobis_0.01', 'Mahalanobis_0.005', 'Mahalanobis_0.002', 'Mahalanobis_0.0014', 'Mahalanobis_0.001', 'Mahalanobis_0.0005']
     
-    # train and measure the performance of Mahalanobis detector
-    list_best_results, list_best_results_index = [], []
     for dataset in dataset_list:
-        print('In-distribution: ', dataset)
-        outf = '/home/anasa2/deep_Mahalanobis_detector/output/' + args.net_type + '_' + dataset + '/'
-        out_list = ['svhn', 'imagenet_resize', 'lsun_resize']
-        if dataset == 'svhn':
-            out_list = ['cifar10', 'imagenet_resize', 'lsun_resize']
-        elif dataset == 'ham10000':
-            #out_list = ['cifar10', 'cifar100', 'svhn', 'imagenet_resize', 'lsun_resize', 'face', 'face_age', 'isic-2017', 'isic-2016'] #face #face_age
-            out_list = ['ham10000-avg-smoothing','ham10000-brightness','ham10000-contrast','ham10000-dilation','ham10000-erosion','ham10000-med-smoothing','ham10000-rotation','ham10000-shift'] #face #face_age
-        elif dataset == 'mpower-rest':
-            out_list = ['mHealth','MotionSense','oodParkinsonsData','mpower-rest']
-        elif dataset == 'mpower-return':
-            out_list = ['mHealth','MotionSense','oodParkinsonsData','mpower-return']
-        elif dataset == 'mpower-outbound':
-            out_list = ['mHealth','MotionSense','oodParkinsonsData','mpower-outbound']
-
         list_best_results_out, list_best_results_index_out = [], []
         for out in out_list:
             print('Out-of-distribution: ', out)
             best_tnr, best_result, best_index = 0, 0, 0
             for score in score_list:
                 total_X, total_Y = lib_regression.load_characteristics(score, dataset, out, outf)
-                #X_val, Y_val, X_test, Y_test = lib_regression.block_split(total_X, total_Y, out)
-                #X_val, Y_val, X_test, Y_test = lib_regression.block_split_adv(total_X, total_Y)
                 X_val, Y_val, X_test, Y_test = lib_regression.stratified_split(total_X, total_Y)
-                #X_train = np.concatenate((X_val[:500], X_val[1000:1500]))
-                #Y_train = np.concatenate((Y_val[:500], Y_val[1000:1500]))
-                #X_val_for_test = np.concatenate((X_val[500:1000], X_val[1500:]))
-                #Y_val_for_test = np.concatenate((Y_val[500:1000], Y_val[1500:]))
-                #partition = int(len(X_val)/2)
-                #X_train = np.concatenate((X_val[:partition], X_val[:partition]))
-                #Y_train = np.concatenate((Y_val[:partition], Y_val[:partition]))
-                #X_val_for_test = np.concatenate((X_val[partition:], X_val[partition:]))
-                #Y_val_for_test = np.concatenate((Y_val[partition:], Y_val[partition:]))
+                
                 X_train, Y_train, X_val_for_test, Y_val_for_test = lib_regression.stratified_split(X_val, Y_val, test_size=0.5)
                 lr = LogisticRegressionCV(n_jobs=-1, max_iter=1000).fit(X_train, Y_train)
                 y_pred = lr.predict_proba(X_train)[:, 1]
-                #print('training mse: {:.4f}'.format(np.mean(y_pred - Y_train)))
+
                 y_pred = lr.predict_proba(X_val_for_test)[:, 1]
-                #print('test mse: {:.4f}'.format(np.mean(y_pred - Y_val_for_test)))
+
                 results = lib_regression.detection_performance(lr, X_val_for_test, Y_val_for_test, outf)
                 if best_tnr < results['TMP']['TNR']:
                     best_tnr = results['TMP']['TNR']
@@ -91,14 +52,6 @@ def train_detector():
 
     for in_list in list_best_results:
         print('in_distribution: ' + dataset_list[count_in] + '==========')
-        out_list = ['svhn', 'imagenet_resize', 'lsun_resize']
-        if dataset_list[count_in] == 'svhn':
-            out_list = ['cifar10', 'imagenet_resize', 'lsun_resize']
-        elif dataset == 'ham10000':
-            #out_list = ['cifar10', 'cifar100', 'svhn', 'imagenet_resize', 'lsun_resize', 'face', 'face_age', 'isic-2017', 'isic-2016']
-            out_list = ['ham10000-avg-smoothing','ham10000-brightness','ham10000-contrast','ham10000-dilation','ham10000-erosion','ham10000-med-smoothing','ham10000-rotation','ham10000-shift']
-        elif dataset == 'mpower-rest':
-            out_list = ['mHealth','MotionSense','oodParkinsonsData','MotionSense','mpower-rest']
             
         count_out = 0
         for results in in_list:
@@ -115,5 +68,5 @@ def train_detector():
             count_out += 1
         count_in += 1
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
