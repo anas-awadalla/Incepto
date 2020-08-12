e4c4a487370a9cb041020453760535d69eb63426
Anas Awadalla <anasa2@cs.washington.edu>
2020-08-12 12:20:26 -0700

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Incepto',
+    version='0.0.1',
+    description='OOD Detection and Visualization for Medical Models',
+    url='git@github.com:rfschubert/ptolemaios-sdk-package.git',
+    author='Anas Awadalla',
+    author_email='anasa2@uw.edu',
+    license='unlicense',
+    packages=['Incepto'],
+    zip_safe=False
+)
