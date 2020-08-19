cce1de5fb923293ba4bb83ffaec11c77d06b3b62
Anas Awadalla <anasa2@cs.washington.edu>
2020-08-19 16:04:34 -0700

    fixed front facing

--- Incepto/client.py
+++ Incepto/client.py
@@ -4,16 +4,17 @@ from OOD_Regression_Mahalanobis import train_detector
 from ADV_Samples import generate_adv_samples
 from datasets.parkinsons_dataset import parkinsonsData
 from models.parkinsonsNet import Network
+from Incepto.gram_ood import gram_metrics
 import os
 import torch
 import torchvision
 from torchvision.transforms import transforms
 
 class client():
-    def __init__(self, pre_trained_net_path, in_distribution, out_of_distribution, data_labels, num_classes, is_image=False, transformation=None):
+    def __init__(self, model, in_distribution, out_of_distribution, data_labels, num_classes, is_image=False, transformation=None):
         self.in_distribution = in_distribution
         self.out_of_distribution = out_of_distribution
-        self.model = pre_trained_net_path
+        self.model = model
         self.num_classes = num_classes
         self.in_transform = transformation
         self.data_lables = data_labels
@@ -25,9 +26,12 @@ class client():
         else:
             analyze(model=self.model, in_distribution=self.in_distribution,out_of_distribution=self.out_of_distribution,channel_labels=channel_labels,signal_frequency=200,data_labels=self.data_lables)
         
-    def detect_ood(self, gpu, batch_size):
-        extract_features(self.model, self.in_distribution, self.data_labels[0], self.data_labels[1:], self.out_of_distribution, self.in_transform, gpu=gpu, batch_size=batch_size, num_classes=self.num_classes)
+    def detect_ood_mahalanobis(self, model, gpu, batch_size):
+        extract_features(model, self.in_distribution, self.data_labels[0], self.data_labels[1:], self.out_of_distribution, self.in_transform, gpu=gpu, batch_size=batch_size, num_classes=self.num_classes)
         train_detector([self.data_labels[0]], self.data_labels[1:])
+        
+    def detect_ood_gram(self, model, gpu, batch_size):
+        gram_metrics(model,self.data_labels[0],self.in_distribution,self.data_labels[1:], self.out_of_distribution,gpu=gpu,batch_size=batch_size)
     
     # def attack(self, adv_type, gpu, batch_size=200, in_transform=None):
     #     generate_adv_samples(model=self.model,net_type="model",dataset_name="mPower",dataset = self.in_distribution,gpu=gpu,adv_type=adv_type,num_classes=self.num_classes,batch_size=batch_size,in_transform=in_transform)
